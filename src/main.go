09dd9ec3dfd0a894587075eb5738896eb3d4f7e6
Bibin Vasudevan <bibinvasudev@gmail.com>
2019-10-22 09:14:13 +0530

    Updated with updation in java script

--- src/main.go
+++ src/main.go
@@ -1,86 +1,52 @@
+// websockets.go
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/websocket"
+    "fmt"
+    "net/http"
+    "time"
+    "github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool) // connected clients
-var broadcast = make(chan Message)           // broadcast channel
-
-// Configure the upgrader
 var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-// Define our message object
-type Message struct {
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Message  string `json:"message"`
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
 }
 
 func main() {
 	// Create a simple file server
-	fs := http.FileServer(http.Dir("../public"))
-	http.Handle("/", fs)
+    fs := http.FileServer(http.Dir("../public"))
+    http.Handle("/", fs)
+    http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+        conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
-	// Configure websocket route
-	http.HandleFunc("/ws", handleConnections)
+        for {
+            // Read message from browser
+            msgType, msg, err := conn.ReadMessage()
+            fmt.Printf(string (msg))
+            if err != nil {
+                return
+            }
 
-	// Start listening for incoming messages
-	go handleMessages()
+            // Print the message to the console
+            fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+            msg = []byte("Here is a string....")
+            // Write message back to browser
+            for i := 0; i < 50; i++ {
+                time.Sleep(time.Second * 1)
+                t := time.Now()
+                //fmt.Println(t.Format("20060102150405"))
+                msg = []byte(t.Format("20060102150405"))
 
-	// Start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Make sure we close the connection when the function returns
-	defer ws.Close()
-
-	// Register our new client
-	clients[ws] = true
-
-	for {
-		var msg Message
-		// Read in a new message as JSON and map it to a Message object
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			break
-		}
-		// Send the newly received message to the broadcast channel
-		broadcast <- msg
-	}
-}
-
-func handleMessages() {
-	for {
-		// Grab the next message from the broadcast channel
-		msg := <-broadcast
-		// Send it out to every client that is currently connected
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-	}
+                if err = conn.WriteMessage(msgType, msg); err != nil {
+                    return
+                }            }
+            msg = []byte("Last string....")
+            conn.WriteMessage(msgType, msg)
+            //if err = conn.WriteMessage(msgType, msg); err != nil {
+            //    return
+            //}
+        }
+    })
+    http.ListenAndServe(":8080", nil)
 }
